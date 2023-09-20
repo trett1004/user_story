4384fe2311bd4cc99b46a1274fcc6f4bbe868066
Max <maxtretter@tutanota.com>
2023-09-20 12:20:01 +0200

    Create data service for api fetch / in directory app reorganize directories

---
+++ src/app/components/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { DataService } from '../services/data-service.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'user_story';
+  apiData: any = [];
+
+  constructor(private dataService: DataService) {}
+
+  ///////////////////////   Fetch data from api   ///////////////////////
+  ngOnInit() {
+    this.accessDataservice();
+  }
+
+  private accessDataservice() {
+    this.dataService.fetchData().subscribe((data: any) => {
+      console.log(data);
+      this.apiData = data;
+    });
+  }
+}
